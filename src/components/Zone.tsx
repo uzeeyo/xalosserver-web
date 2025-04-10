1bdbfcd53e3bf24c588b0a28b05381eee8d3a7ef
Jordan Minott <jgminott@gmail.com>
2025-04-09 22:12:16 -0400

    Sort zones by id

--- src/components/Zone.tsx
+++ src/components/Zone.tsx
@@ -1,13 +1,15 @@
 interface ZoneProps {
   children?: React.ReactNode;
+  id: number;
 }
 
-function Zone({children }: ZoneProps) {
+function Zone({children, id}: ZoneProps) {
   return (
     <div
-      className="h-[12rem] p-2 min-w-32 lg:min-w-48 w-56 bg-gray-800 rounded-lg flex justify-center"
+      className="h-[12rem] p-2 min-w-32 lg:min-w-48 w-56 bg-gray-800 rounded-lg flex justify-center relative"
     >
       {children}
+      <div className="absolute bottom-0 left-1/2 text-xs">{id}</div>
     </div>
   );
 }
